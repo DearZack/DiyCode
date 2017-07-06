6460e59582f4b5e4a9382b48a0ff8ce8e860708b
DearZack <zack4work@gmail.com>
2017-07-06 20:47:41 +0800

    PersonalActivity布局

--- app/src/main/java/io/github/dearzack/diycode/personal/PersonalActivity.java
+++ app/src/main/java/io/github/dearzack/diycode/personal/PersonalActivity.java
@@ -1,9 +1,19 @@
 package io.github.dearzack.diycode.personal;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.dearzack.diycode.R;
 import io.github.dearzack.diycode.base.BaseActivity;
 
@@ -12,16 +22,34 @@ public class PersonalActivity extends BaseActivity implements PersonalDetailCont
     public static final String LOGIN_NAME = "LOGIN_NAME";
     @Inject
     PersonalDetailPresenter presenter;
+    @BindView(R.id.avatar)
+    ImageView avatar;
+    @BindView(R.id.head_layout)
+    RelativeLayout headLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsing_toolbar_layout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.list)
+    RecyclerView list;
+    @BindView(R.id.nsv)
+    NestedScrollView nsv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+        ButterKnife.bind(this);
         DaggerPersonalDetailComponent.builder()
                 .personalDetailPresenterModule(new PersonalDetailPresenterModule(this))
                 .build()
                 .inject(this);
         toast(getIntent().getStringExtra(LOGIN_NAME));
+        collapsingToolbarLayout.setTitle("周雄");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -39,5 +67,15 @@ public class PersonalActivity extends BaseActivity implements PersonalDetailCont
     @Override
     public void setPresenter(PersonalDetailContract.Presenter presenter) {
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/io/github/dearzack/diycode/widget/ImageBehavior.java
@@ -0,0 +1,46 @@
+package io.github.dearzack.diycode.widget;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+
+import io.github.dearzack.diycode.R;
+
+/**
+ * Created by Zack on 2017/7/6.
+ */
+
+public class ImageBehavior extends CoordinatorLayout.Behavior<View> {
+
+    private float threshold = 0;
+
+    public ImageBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        Log.e("zhouxiong", "layoutDependsOn");
+        return dependency.getId() == R.id.app_bar_layout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        if (threshold == 0) {
+            threshold = child.getContext().getResources().getDimension(R.dimen.personal_threshold);
+        }
+        Log.e("zhouxiong", "Y:" + dependency.getY());
+        float dependencyY = Math.abs(dependency.getY());
+        float precent = Math.min(dependencyY / threshold, 1);
+        Log.e("zhouxiong", "precent" + precent);
+//        child.getBackground().setAlpha((int) (255 * (1 - precent)));
+        child.setScaleX(1 - precent);
+        child.setScaleY(1 - precent);
+        child.setAlpha(1 - precent);
+        return super.onDependentViewChanged(parent, child, dependency);
+    }
+}
